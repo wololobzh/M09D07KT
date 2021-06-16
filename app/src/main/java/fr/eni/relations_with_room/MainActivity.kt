9dd7a674f133c63e2581f4f6715c600e582d93a3
cosson <acosson@eni-ecole.fr>
2021-06-16 20:05:25 +0200

    Test et config avec Stetho

--- app/src/main/java/fr/eni/relations_with_room/MainActivity.kt
+++ app/src/main/java/fr/eni/relations_with_room/MainActivity.kt
@@ -1,11 +1,39 @@
 package fr.eni.relations_with_room
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import com.facebook.stetho.Stetho
+import fr.eni.relations_with_room.dao.UserDao
+import fr.eni.relations_with_room.dao.utils.AppDatabase
+import fr.eni.relations_with_room.model.Address
+import fr.eni.relations_with_room.model.User
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.ThreadLocalRandom
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userDao: UserDao
+    private lateinit var db: AppDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Stetho.initializeWithDefaults(this);
+
         setContentView(R.layout.activity_main)
+
+        var userDao = AppDatabase.getInstance(application).userDao()
+
+        GlobalScope.launch{
+            userDao.insert(User(20,"XXX","XXX", Address("Alma","Britany","Rennes",35000)))
+        }
+
     }
 }
